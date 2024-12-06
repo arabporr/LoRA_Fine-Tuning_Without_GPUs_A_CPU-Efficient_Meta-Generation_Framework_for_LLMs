8fcda11dce5000c376e8074d8608e28bfaed0666
Reza Arabpour <arabporr@yahoo.com>
2024-12-06 07:59:28 -0500

    MMD added

--- 2_distances.py
+++ 2_distances.py
@@ -62,6 +62,52 @@ def JS_Divergence(f1_samples, f2_samples, index_1, index_2):
     return js_div
 
 
+def MMD(f1_samples, f2_samples, index_1, index_2, kernel: str = "multiscale"):
+    """Emprical maximum mean discrepancy. The lower the result
+       the more evidence that distributions are the same.
+
+    Args:
+        x: first sample, distribution P
+        y: second sample, distribution Q
+        kernel: kernel type such as "multiscale" or "rbf"
+    """
+    x = torch.tensor([np.random.choice(f1_samples, 1000)])
+    y = torch.tensor([np.random.choice(f2_samples, 1000)])
+    xx = torch.mm(x.t(), x)
+    yy = torch.mm(y.t(), y)
+    zz = torch.mm(x.t(), y)
+    rx = xx.diag().unsqueeze(0).expand_as(xx)
+    ry = yy.diag().unsqueeze(0).expand_as(yy)
+
+    dxx = rx.t() + rx - 2.0 * xx  # Used for A in (1)
+    dyy = ry.t() + ry - 2.0 * yy  # Used for B in (1)
+    dxy = rx.t() + ry - 2.0 * zz  # Used for C in (1)
+
+    XX, YY, XY = (
+        torch.zeros(xx.shape),
+        torch.zeros(xx.shape),
+        torch.zeros(xx.shape),
+    )
+
+    if kernel == "multiscale":
+
+        bandwidth_range = [0.2, 0.5, 0.9, 1.3]
+        for a in bandwidth_range:
+            XX += a**2 * (a**2 + dxx) ** -1
+            YY += a**2 * (a**2 + dyy) ** -1
+            XY += a**2 * (a**2 + dxy) ** -1
+
+    if kernel == "rbf":
+
+        bandwidth_range = [10, 15, 20, 50]
+        for a in bandwidth_range:
+            XX += torch.exp(-0.5 * dxx / a)
+            YY += torch.exp(-0.5 * dyy / a)
+            XY += torch.exp(-0.5 * dxy / a)
+
+    return torch.mean(XX + YY - 2.0 * XY)
+
+
 #### Setting Distance Measure
 if distance == "WD":
     distance_metric = W_Distance
@@ -73,6 +119,9 @@ elif distance == "KL":
 elif distance == "JS":
     pdf_maker()
     distance_metric = JS_Divergence
+    dist_func_is_symmetric = True
+elif distance == "MMD":
+    distance_metric = MMD
     dist_func_is_symmetric = True
 
 
@@ -116,8 +165,8 @@ def Distance_Calculator(base_index: int) -> str:
 
             torch.save(Distance_Vectors[base_index], distance_file_path)
             return f"Done making base dataset: {base_index}"
-        except:
-            return f"Error in making base dataset: {base_index}"
+        except Exception as e:
+            return f"Error in making base dataset: {base_index}\n\t{e}"
     else:
         try:
             distances = torch.load(distance_file_path, weights_only=False)
